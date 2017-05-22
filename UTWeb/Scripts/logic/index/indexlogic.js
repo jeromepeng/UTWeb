63813ffca476682f0f1c6501e064b40493514f89
jerome peng <snowxbear@163.com>
2017-05-22 20:32:38 +0800

    Add feature: Run JS on line.

--- UTWeb/Scripts/logic/index/indexlogic.js
+++ UTWeb/Scripts/logic/index/indexlogic.js
@@ -2,16 +2,23 @@
 function VMIndex() {
     var self = this;
     self.isShowButton = ko.observable(false);//observable property shouldn't declare as "var isShowButton = ko.observable(false);", if do so, the code "self.isShowButton(true);" won't work
-    self.OnChange = function () {
-        var selectString = $('#AppType').val();
+    self.onChange = function () {
+        var selectString = $('#appType').val();
         if (selectString === 'JavaScript') {
-            alert('JS');
             self.isShowButton(true);//should be like this rather than "self.isShowButton = ko.observable(true);"
         }
         else if (selectString === 'DLL') {
-            alert('Dll');
             self.isShowButton(false);
         };
     }
+    self.runJS = function() {
+        var myScript = document.createElement("script");
+        myScript.type = "text/javascript";
+        myScript.appendChild(document.createTextNode($('#jsTextArea').val()));
+        document.body.appendChild(myScript);
+        //此处发现可以运行
+        mainFunction();
+    };
 }
 ko.applyBindings(new VMIndex());
+
