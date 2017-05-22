a099530379857ea60a1a45c2bbae72b1e448eba0
jerome peng <snowxbear@163.com>
2017-05-22 20:13:50 +0800

    Fix some bug, it can work now. And add some comments.

--- UTWeb/Scripts/logic/index/indexlogic.js
+++ UTWeb/Scripts/logic/index/indexlogic.js
@@ -1,23 +1,17 @@
 ﻿
-
-function IndexVM() {
-    this.ShowUploadButton = ko.observable(false);
-
-    var OnChange = function () {
+function VMIndex() {
+    var self = this;
+    self.isShowButton = ko.observable(false);//observable property shouldn't declare as "var isShowButton = ko.observable(false);", if do so, the code "self.isShowButton(true);" won't work
+    self.OnChange = function () {
         var selectString = $('#AppType').val();
         if (selectString === 'JavaScript') {
             alert('JS');
-            ThisVM.ShowUploadButton = ko.observable(true);
+            self.isShowButton(true);//should be like this rather than "self.isShowButton = ko.observable(true);"
         }
         else if (selectString === 'DLL') {
             alert('Dll');
-            ThisVM.ShowUploadButton = ko.observable(false);
+            self.isShowButton(false);
         };
-    };
+    }
 }
-
-var ThisVM = new IndexVM();
-
-
-
-ko.applyBindings(ThisVM);
+ko.applyBindings(new VMIndex());
